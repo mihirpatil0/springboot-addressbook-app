69af94393ae25a9e1bb424085cb442b9cafe940a
mihirpatil0 <mihirrpatil@gmail.com>
2021-09-05 07:43:19 +0530

    [mihir] Added: new annotations that autocreate tables into mysql.

--- src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
+++ src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
@@ -3,10 +3,19 @@ package com.bridgelabz.addressbookapp.model;
 import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
 import lombok.Data;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "addressbook")
 @Data
 public class AddressBookData {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "person_id")
     private int personId;
+
+    @Column(name = "fullname")
     private String fullName;
     private String address;
     private String city;
@@ -16,8 +25,11 @@ public class AddressBookData {
 
     public AddressBookData() {}
 
-    public AddressBookData(int personId, AddressBookDTO addressBookDTO){
-        this.personId = personId;
+    public AddressBookData(AddressBookDTO addressBookDTO){
+        this.updateAddressBookData(addressBookDTO);
+    }
+
+    public void updateAddressBookData(AddressBookDTO addressBookDTO){
         this.fullName = addressBookDTO.fullName;
         this.address = addressBookDTO.address;
         this.city = addressBookDTO.city;
